b63e065b1347af9a06f7974495e8824bec91128f
Liliana Kastilio <kastilio.liliana@gmail.com>
2015-09-27 23:23:52 +0100

    Added map

--- src/js/map.js
+++ src/js/map.js
@@ -1,20 +1,14 @@
-var map;
-
-function initialize() {
-  var myLatlng = new google.maps.LatLng(53.260998,-3.501757);
-  var mapOptions = {
-    zoom: 19,
-    center: myLatlng
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-  
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title: 'Listit'
-  });
-  
+function initialize_map(a, b) {
+    var c = new google.maps.LatLng(a, b),
+        d = {
+            zoom: 9,
+            center: c
+        };
+    map = new google.maps.Map(document.getElementById("map-canvas"), d), new google.maps.Marker({
+        position: c,
+        map: map,
+        title: "Listit"
+    })
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+var map;
