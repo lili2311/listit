f95432639cdefd2a63136a1a98f544dcce1088da
Liliana Kastilio <kastilio.liliana@gmail.com>
2015-09-27 22:38:41 +0100

    Adding src, tweaked design and listing page mockup

---
+++ src/js/map.js
@@ -0,0 +1,20 @@
+var map;
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(53.260998,-3.501757);
+  var mapOptions = {
+    zoom: 19,
+    center: myLatlng
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Listit'
+  });
+  
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
